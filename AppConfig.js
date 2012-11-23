a8e33b08ae936b6db4b45beea06b32949d660c94
OpenShift guest <mooneer@gmail.com>
2012-11-23 14:44:21 -0500

    Set up Nodemailer in project.

--- AppConfig.js
+++ AppConfig.js
@@ -1,6 +1,8 @@
 // postShuffle -- web forum software for node.js
 // Copyright (c) 2012 Mooneer Salem
 
+var nodemailer = require("nodemailer");
+
 var debugMode = true;
 
 var settings = function() {
@@ -25,6 +27,11 @@ var settings = function() {
     
     this.sessionSecret = "cookie monster";
     this.defaultTitle = "User";
+    
+    // Perform initialization here so that we have a mailer accessible from
+    // anywhere in the project. See Nodemailer documentation for setup
+    // instructions.
+    this.mailer = nodemailer.createTransport("sendmail");
 };
 
 module.exports = new settings();
