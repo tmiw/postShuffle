f937c0ed3e255e2e571909ef5f1618879128edc2
OpenShift guest <mooneer@gmail.com>
2013-03-05 03:05:52 -0500

    fixed first start crash

--- DataModel.js
+++ DataModel.js
@@ -43,4 +43,31 @@ exports.Tags.hasMany(exports.Posts);
 exports.Comments.hasOne(exports.Comments, {'as': 'Parent'});
 exports.Comments.belongsTo(exports.Users);
 
-db.sync();
+var Passwords = require(__dirname + "/Utility/Passwords.js");
+var generator = require(__dirname + "/Utility/RandomGenerators.js");
+db.sync().success(function() {
+    exports.Users.findAll({limit: 1}).success(function(result) {
+        if (!result || result.length === 0)
+        {
+            // first run.
+            var newpw = generator.randomString();
+            exports.Users.create({
+                username: "admin",
+                password: Passwords.hash(newpw),
+                is_moderator: true,
+                is_admin: true,
+                title: AppConfig.defaultTitle,
+                email: AppConfig.admin_email
+            }).success(function(u) {
+                console.log("============= FIRST RUN  =============");
+                console.log("Your admin user is as follows:");
+                console.log("   Username: admin");
+                console.log("   Password: " + newpw);
+                console.log("KEEP THIS INFO IN A SAFE PLACE OR YOU WILL BE UNABLE TO ADMINISTER YOUR FORUM.");
+                console.log("============= /FIRST RUN =============");
+            }).error(function(err) {
+                console.log("Cannot access database: " + err);
+            });
+        }
+    });
+});
